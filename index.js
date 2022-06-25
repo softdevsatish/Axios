18b249314c33b3eef5b51fd8a2bcd95c91ab76ae
Satish Jaiswal <89683535+softdevsatish@users.noreply.github.com>
2022-06-25 14:54:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // import node-fetch
 const axios = require('axios');
 // set url as constant
-const URL = 'https://api.hubkoin.com:8443/api/public/config';
+const URL = 'https://jsonplaceholder.typicode.com/posts';
 
 axios
     .get(URL)
